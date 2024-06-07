0ad4ef40c5d4914ebe808e1d05d93c54fd0e2e75
HuntFeng <spacecurvature@gmail.com>
2024-06-07 01:59:14 -0600

    better WinSeparator color

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -10,6 +10,9 @@ return {
 				sidebars = "transparent",
 				floats = "transparent",
 			},
+			on_colors = function(colors)
+				colors.border = colors.blue
+			end,
 		})
 		vim.cmd("colorscheme tokyonight")
 	end,
