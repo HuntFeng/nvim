c7daed0311845d2943e4a9fe2d7d5b7f57ff9e97
HuntFeng <spacecurvature@gmail.com>
2024-07-02 22:54:35 -0600

    better snippet behavior for tex file

--- ftplugin/tex.lua
+++ ftplugin/tex.lua
@@ -1,8 +1,16 @@
+-- remove Function types from the suggestion list as they are unnecessary
+-- so that snippets will appear in the suggestion list
 local cmp = require("cmp")
 local sources = cmp.get_config().sources
 for i = #sources, 1, -1 do
 	if sources[i].name == "nvim_lsp" then
-		table.remove(sources, i)
+		sources[i].entry_filter = function(entry, ctx)
+			local kind = require("cmp.types").lsp.CompletionItemKind[entry:get_kind()]
+			if kind == "Function" then
+				return false
+			end
+			return true
+		end
 	end
 end
 cmp.setup.buffer({ sources = sources })
--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -13,10 +13,10 @@ return {
 	event = "InsertEnter",
 	config = function()
 		-- define keymaps for autocomplete
+		require("luasnip.loaders.from_vscode").lazy_load({ paths = { vim.fn.stdpath("config") .. "/snippets" } }) -- load custom snippets
+		require("luasnip.loaders.from_vscode").lazy_load() -- load friendly-snippets
 		local cmp = require("cmp")
 		local cmp_format = require("lsp-zero").cmp_format({ details = true })
-		require("luasnip.loaders.from_vscode").lazy_load() -- load friendly-snippets
-		require("luasnip.loaders.from_vscode").lazy_load({ paths = { vim.fn.stdpath("config") .. "/snippets" } }) -- load custom snippets
 		cmp.setup({
 			window = {
 				completion = cmp.config.window.bordered(),
@@ -30,12 +30,13 @@ return {
 				["<Tab>"] = cmp.mapping.select_next_item({ behavior = "select" }),
 			},
 			sources = {
-				{ name = "luasnip" },
 				{ name = "nvim_lsp" },
+				{ name = "luasnip" },
 				{ name = "buffer" },
 				{ name = "path" },
 			},
 			snippet = {
+				-- required - must specify a snippet engine
 				expand = function(args)
 					require("luasnip").lsp_expand(args.body)
 				end,
