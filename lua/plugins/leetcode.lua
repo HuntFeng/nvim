c1bbc6c4175fc804055f95ba90a00c7dfeb4035b
HuntFeng <spacecurvature@gmail.com>
2024-07-12 09:02:05 -0600

    added leetcode.nvim

---
+++ lua/plugins/leetcode.lua
@@ -0,0 +1,13 @@
+return {
+	"kawre/leetcode.nvim",
+	build = ":TSUpdate html",
+	dependencies = {
+		"nvim-telescope/telescope.nvim",
+		"nvim-lua/plenary.nvim", -- required by telescope
+		"MunifTanjim/nui.nvim",
+	},
+	opts = {
+		lang = "cpp",
+		image_support = true,
+	},
+}
