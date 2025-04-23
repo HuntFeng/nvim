67b82db99833dd77430d7d1a6e88b2e7f42004a6
HuntFeng <spacecurvature@gmail.com>
2025-04-22 22:18:28 -0600

    separate dap and added lazy event

--- lua/plugins/flutter.lua
+++ lua/plugins/flutter.lua
@@ -1,10 +1,10 @@
 return {
 	{
 		"nvim-flutter/flutter-tools.nvim",
-		lazy = false,
 		dependencies = {
 			"nvim-lua/plenary.nvim",
 		},
+		ft = { "dart" },
 		opts = {
 			lsp = {
 				settings = { lineLength = 120 },
@@ -18,49 +18,8 @@ return {
 		},
 	},
 	{
-		"mfussenegger/nvim-dap",
-		dependencies = {
-			"rcarriga/nvim-dap-ui",
-			"nvim-neotest/nvim-nio",
-		},
-		config = function()
-			local dap, dapui = require("dap"), require("dapui")
-			dapui.setup()
-			dap.listeners.before.attach.dapui_config = function()
-				dapui.open()
-			end
-			dap.listeners.before.launch.dapui_config = function()
-				dapui.open()
-			end
-			dap.listeners.before.event_terminated.dapui_config = function()
-				dapui.close()
-			end
-			dap.listeners.before.event_exited.dapui_config = function()
-				dapui.close()
-			end
-
-			vim.keymap.set("n", "<leader>dt", function()
-				dapui.toggle()
-			end, { desc = "Toggle DAP UI" })
-			vim.keymap.set("n", "<leader>db", function()
-				dap.toggle_breakpoint()
-			end, { desc = "Dap Toggle Breakpoint" })
-			vim.keymap.set("n", "<leader>dC", function()
-				dap.clear_breakpoints()
-			end, { desc = "Dap Clear Breakpoints" })
-			vim.keymap.set("n", "<leader>di", function()
-				dap.step_into()
-			end, { desc = "Dap Step Into" })
-			vim.keymap.set("n", "<leader>do", function()
-				dap.step_out()
-			end, { desc = "Dap Step Out" })
-			vim.keymap.set("n", "<leader>dc", function()
-				dap.continue()
-			end, { desc = "Dap Continue" })
-		end,
-	},
-	{
 		"vim-test/vim-test",
+		event = "VeryLazy",
 		config = function()
 			local dap = require("dap")
 
@@ -120,6 +79,7 @@ return {
 			"antoinemadec/FixCursorHold.nvim",
 			"nvim-neotest/neotest-vim-test",
 		},
+		event = "VeryLazy",
 		config = function()
 			require("neotest").setup({
 				adapters = {
