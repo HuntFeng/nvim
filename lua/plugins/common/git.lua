cd0792a4bdd143594a98474564e8551783865c78
huntf-bitstrata <hunt.feng@bitstrata.com>
2025-04-24 08:19:12 -0600

    added lazy event to diffview

--- lua/plugins/common/git.lua
+++ lua/plugins/common/git.lua
@@ -11,6 +11,7 @@ return {
 	},
 	{
 		"sindrets/diffview.nvim",
+		lazy = true,
 		keys = {
 			{ "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "Diffview" },
 			{ "<leader>gc", "<cmd>DiffviewClose<cr>", desc = "Diffview Close" },
