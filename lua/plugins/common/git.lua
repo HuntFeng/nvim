3c035d9fa9b70f21bda49c2c91ccf29140e304bb
huntf-bitstrata <hunt.feng@bitstrata.com>
2025-04-24 08:17:30 -0600

    added git stuff

---
+++ lua/plugins/common/git.lua
@@ -0,0 +1,19 @@
+return {
+	{
+		"kdheepak/lazygit.nvim",
+		lazy = true,
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		keys = {
+			{ "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+		},
+	},
+	{
+		"sindrets/diffview.nvim",
+		keys = {
+			{ "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "Diffview" },
+			{ "<leader>gc", "<cmd>DiffviewClose<cr>", desc = "Diffview Close" },
+		},
+	},
+}
