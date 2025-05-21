73105946fb25ed29710f2be3bd84eb702e669d29
HuntFeng <spacecurvature@gmail.com>
2025-05-20 23:42:28 -0600

    improved ui

--- lua/plugins/common/snipe.lua
+++ lua/plugins/common/snipe.lua
@@ -10,6 +10,10 @@ return {
 		},
 	},
 	opts = {
+		ui = {
+			position = "center",
+			text_align = "file-first",
+		},
 		hints = {
 			-- Charaters to use for hints (NOTE: make sure they don't collide with the navigation keymaps)
 			dictionary = "qwertyuiop",
