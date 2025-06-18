31f2a4f4a2c4cbc721ef68a36eae717d3a3c5779
HuntFeng <spacecurvature@gmail.com>
2025-06-18 10:27:00 -0600

    upate installation script

--- install.sh
+++ install.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
-# install xclip for system clipboard
-sudo apt-get install -y xclip
-# install ripgrep for searching in files
-sudo apt-get install -y ripgrep
-# install nodejs and npm for copilot 
-curl -fsSL https://deb.nodesource.com/setup_20.x | sudo bash - && sudo apt-get install -y nodejs
-# install neovim
-cd /opt
-sudo wget https://github.com/neovim/neovim/releases/download/v0.11.1/nvim-linux-x86_64.tar.gz
-sudo mkdir nvim && sudo tar -xzvf nvim-linux-x86_64.tar.gz -C nvim --strip-components=1
-sudo ln -s /opt/nvim/bin/nvim /usr/bin/nvim
+# run this script as root
+
+# xclip for system clipboard
+# ripgrep for searching
+# python for some language servers
+# n for nodejs and npm (for some language servers)
+apt-get update \
+&& apt-get install -y xclip \
+&& apt-get install -y ripgrep \
+&& apt-get install -y python3 python3-pip python3-venv \
+&& curl -fsSL https://raw.githubusercontent.com/tj/n/master/bin/n | bash -s install lts \
+&& cd /opt \
+&& wget https://github.com/neovim/neovim/releases/download/v0.11.1/nvim-linux-x86_64.tar.gz \
+&& mkdir nvim \
+&& tar -xzvf nvim-linux-x86_64.tar.gz -C nvim --strip-components=1 \
+&& ln -s /opt/nvim/bin/nvim /usr/bin/nvim \
+&& rm -rf /opt/nvim-linux-x86_64.tar.gz
 
