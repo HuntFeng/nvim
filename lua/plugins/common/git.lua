02dc550ee12d1a0be081acac7beed078e59d1418
HuntFeng <spacecurvature@gmail.com>
2025-09-26 14:52:37 -0600

    not really using lazygit in neovim

--- lua/plugins/common/git.lua
+++ lua/plugins/common/git.lua
@@ -1,20 +1,8 @@
 return {
-	{
-		"kdheepak/lazygit.nvim",
-		lazy = true,
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-		},
-		keys = {
-			{ "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
-		},
-	},
-	{
-		"sindrets/diffview.nvim",
-		lazy = true,
-		keys = {
-			{ "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "Diffview" },
-			{ "<leader>gc", "<cmd>DiffviewClose<cr>", desc = "Diffview Close" },
-		},
+	"sindrets/diffview.nvim",
+	lazy = true,
+	keys = {
+		{ "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "Diffview" },
+		{ "<leader>gc", "<cmd>DiffviewClose<cr>", desc = "Diffview Close" },
 	},
 }
