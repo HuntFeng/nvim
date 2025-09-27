3d938b77bcd433aaa98fb73f2c52900fc7d4b3fd
HuntFeng <spacecurvature@gmail.com>
2025-09-26 20:11:36 -0600

    not really using diffview

--- lua/plugins/common/git.lua
+++
@@ -1,8 +0,0 @@
-return {
-	"sindrets/diffview.nvim",
-	lazy = true,
-	keys = {
-		{ "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "Diffview" },
-		{ "<leader>gc", "<cmd>DiffviewClose<cr>", desc = "Diffview Close" },
-	},
-}
