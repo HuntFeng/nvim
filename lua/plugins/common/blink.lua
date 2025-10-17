198f43e9ccb8da5e5a5d4c898c3ec5efba6384ba
HuntFeng <spacecurvature@gmail.com>
2025-10-17 09:44:36 -0600

    better blink keybinds

--- lua/plugins/common/blink.lua
+++ lua/plugins/common/blink.lua
@@ -25,23 +25,20 @@ return {
 		},
 		version = "1.*",
 		opts = {
-			-- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
-			-- 'super-tab' for mappings similar to vscode (tab to accept)
-			-- 'enter' for enter to accept
-			-- 'none' for no mappings
-			--
 			-- All presets have the following mappings:
-			-- C-space: Open menu or open docs if already open
-			-- C-n/C-p or Up/Down: Select next/previous item
+			-- C-space: Open menu or open docs
+			-- C-n/C-p or Up/Down or Tab/S-Tab: Select next/previous item
 			-- C-e: Hide menu
-			-- C-k: Toggle signature help (if signature.enabled = true)
-			--
+			-- C-k: Toggle signature help
+			-- C-y/<CR> to accept
 			-- See :h blink-cmp-config-keymap for defining your own keymap
 			keymap = {
 				preset = "default",
-				["<CR>"] = { "select_and_accept", "fallback" },
+				["<CR>"] = { "accept", "fallback" },
 				["<C-u>"] = { "scroll_documentation_up", "fallback" },
 				["<C-d>"] = { "scroll_documentation_down", "fallback" },
+				["<Tab>"] = { "snippet_forward", "select_next", "fallback" },
+				["<S-Tab>"] = { "snippet_backward", "select_prev", "fallback" },
 			},
 			completion = {
 				documentation = { auto_show = true },
