437d721e886fcbc9e1d22223caf3b9f3c04d5da9
HuntFeng <spacecurvature@gmail.com>
2025-10-25 20:36:31 -0600

    remove lots of unnecessary plugins and remove profile selection

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -46,17 +46,17 @@ return {
 			end
 
 			-- Keymaps
-			vim.keymap.set("n", "K", vim.lsp.buf.hover)
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition)
-			vim.keymap.set("n", "gD", vim.lsp.buf.declaration)
-			vim.keymap.set("n", "grr", vim.lsp.buf.references)
-			vim.keymap.set("n", "gri", vim.lsp.buf.implementation)
-			vim.keymap.set("n", "gra", vim.lsp.buf.code_action)
-			vim.keymap.set("n", "grn", vim.lsp.buf.rename)
-			vim.keymap.set("n", "gO", vim.lsp.buf.outgoing_calls)
-			vim.keymap.set("n", "gI", vim.lsp.buf.incoming_calls)
-			vim.keymap.set("n", "gl", vim.diagnostic.open_float)
-			vim.keymap.set("n", "<C-s>", vim.lsp.buf.signature_help)
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Hover" })
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Goto definition" })
+			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { desc = "Goto declaration" })
+			vim.keymap.set("n", "grr", vim.lsp.buf.references, { desc = "Goto references" })
+			vim.keymap.set("n", "gri", vim.lsp.buf.implementation, { desc = "Goto implementation" })
+			vim.keymap.set("n", "gra", vim.lsp.buf.code_action, { desc = "Code action" })
+			vim.keymap.set("n", "grn", vim.lsp.buf.rename, { desc = "Rename symbol" })
+			vim.keymap.set("n", "gO", vim.lsp.buf.outgoing_calls, { desc = "Goto outgoing calls" })
+			vim.keymap.set("n", "gI", vim.lsp.buf.incoming_calls, { desc = "Goto incoming calls" })
+			vim.keymap.set("n", "gl", vim.diagnostic.open_float, { desc = "Open diagnostic" })
+			vim.keymap.set("i", "<C-s>", vim.lsp.buf.signature_help, { desc = "Signature help" })
 		end,
 	},
 	-- mason and mason-tool-installer for easier installation of lsp servers and formatters
