514ee0531d9c50f81006e9df54201669e66be03c
HuntFeng <spacecurvature@gmail.com>
2025-11-04 22:30:52 -0600

    netrw is too bugy

--- lua/core/explorer.lua
+++
@@ -1,4 +0,0 @@
-vim.g.netrw_liststyle = 3
-vim.g.netrw_keepdir = 0
-
-vim.keymap.set("n", "<leader>e", "<cmd>Explore .<cr>", { desc = "Netrw at Project Root" })
---
+++ lua/plugins/neotree.lua
@@ -0,0 +1,50 @@
+return {
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"MunifTanjim/nui.nvim",
+		"nvim-tree/nvim-web-devicons",
+	},
+	lazy = true,
+	keys = {
+		{
+			"<leader>e",
+			function()
+				require("neo-tree.command").execute({ toggle = true, reveal = true })
+			end,
+			desc = "Neotree Toggle",
+		},
+	},
+	config = function()
+		require("neo-tree").setup({
+			window = {
+				position = "current",
+				-- use popup to set width for floating window
+				popup = {
+					size = {
+						width = 35,
+					},
+				},
+				mappings = {
+					["h"] = "close_node",
+					["l"] = "open",
+				},
+			},
+			default_component_configs = {
+				git_status = {
+					symbols = {
+						added = "N",
+						modified = "M",
+						deleted = "D",
+						untracked = "",
+						ignored = "",
+						unstaged = "",
+						staged = "",
+						conflict = "",
+					},
+				},
+			},
+		})
+	end,
+}
