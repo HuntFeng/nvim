aca4744712a0e7bc62b0980359e360ec20c5810d
HuntFeng <spacecurvature@gmail.com>
2026-02-26 14:52:03 -0800

    use textobject for more advanced operation

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,13 +1,32 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	event = "VeryLazy",
-	config = function()
-		local configs = require("nvim-treesitter.configs")
-		configs.setup({
-			-- ensure_installed = { "vimdoc", "lua", "python", "javascript", "typescript", "html", "css", "vue" },
-			highlight = { enable = true },
-			indent = { enable = true, disable = { "cpp" } },
-		})
-	end,
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		event = "VeryLazy",
+		config = function()
+			local configs = require("nvim-treesitter.configs")
+			configs.setup({
+				-- ensure_installed = { "vimdoc", "lua", "python", "javascript", "typescript", "html", "css", "vue" },
+				highlight = { enable = true },
+				indent = { enable = true, disable = { "cpp" } },
+			})
+		end,
+	},
+	{
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		config = function()
+			vim.keymap.set({ "x", "o" }, "am", function()
+				require("nvim-treesitter-textobjects.select").select_textobject("@function.outer", "textobjects")
+			end)
+			vim.keymap.set({ "x", "o" }, "im", function()
+				require("nvim-treesitter-textobjects.select").select_textobject("@function.inner", "textobjects")
+			end)
+			vim.keymap.set({ "x", "o" }, "ac", function()
+				require("nvim-treesitter-textobjects.select").select_textobject("@class.outer", "textobjects")
+			end)
+			vim.keymap.set({ "x", "o" }, "ic", function()
+				require("nvim-treesitter-textobjects.select").select_textobject("@class.inner", "textobjects")
+			end)
+		end,
+	},
 }
